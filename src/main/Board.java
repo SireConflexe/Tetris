9b24bc9644e091f7bc003560e7300becd4d8ea48
Thibaud Germain <39822686+Grimnir777@users.noreply.github.com>
2019-04-19 09:08:16 +0200

    floodFill 1st

--- src/main/Board.java
+++ src/main/Board.java
@@ -1,15 +1,22 @@
 package main;
 
 public class Board {
-
+	public enum caseState{EMPTY,FILLED};
+	
 	private int columns, rows;
 	private char boardgame[][];
+	private int pivotX,pivotY;
+	private Forms forms;
 	
 	public Board(int c, int r) {
 		
 		this.columns=c;
 		this.rows=r;
 		this.boardgame= new char[r][c];
+		this.pivotX = (int) c/2;
+		this.pivotY = 0;
+		
+		this.forms = new Forms();
 		
 		//test affichage
 		
@@ -42,7 +49,44 @@ public class Board {
 		System.out.println("|______________________________|");
 	}
 	
+	void flood(int i, int j, int px, int py,String formKey, char value, boolean[][] visited)
+	{
+	    if(px < 0 || px >= this.columns || py < 0 || py >= this.rows || visited[px][py] || this.forms.allForms.get(formKey).template[px][py] == '0')
+	        return;
+	 
+	    visited[px][py] = true;
+	    boardgame[j][i] = value;
+	 
+	    flood(i, j - 1, px, py - 1, formKey, value, visited);
+	    flood(i + 1, j, px + 1, py, formKey, value, visited);
+	    flood(i, j + 1, px, py + 1, formKey, value, visited);
+	    flood(i - 1, j, px - 1, py, formKey, value, visited);
+	}
 	
+	void floodFill(int i, int j, int px, int py, String formKey, char value)
+	{
+	    boolean [][] visited = new boolean[this.columns][this.rows];
+	 
+	    for(int l = 0; l < this.columns; ++l)
+	        for(int m = 0; m < this.rows; ++m)
+	            visited[l][m] = false;
+	 
+	    flood(i, j, px, py, formKey, value, visited);
+	}
+	
+	void drawForm(String formKey){
+		
+		int x = this.forms.allForms.get(formKey).getPosX();
+		int y = this.forms.allForms.get(formKey).getPosxY();
+		floodFill(x, y, this.pivotX, this.pivotY, formKey, 'X');
+	}
+	void clearPiece(String formKey)
+	{
+		int x = this.forms.allForms.get(formKey).getPosX();
+		int y = this.forms.allForms.get(formKey).getPosxY();
+	 
+	    floodFill(x, y, this.pivotX, this.pivotY, formKey, ' ');
+	}
 	public void addForm(int pos_x, int pos_y) {
 		
 		this.boardgame[pos_x][pos_y]='X';
@@ -52,6 +96,7 @@ public class Board {
 		
 		
 		Board b = new Board(10,24);
+		
 		b.displayBoard();
 		
 		
--- src/main/Form.java
+++ src/main/Form.java
@@ -2,9 +2,16 @@ package main;
 
 public class Form {
 	char [][] template;
+	private int posX,posxY;
 	Form(char[][] template)
 	{
 		this.template = template;
+	}
+	public int getPosX() {
+		return posX;
+	}
+	public int getPosxY() {
+		return posxY;
 	}
 	
 }
