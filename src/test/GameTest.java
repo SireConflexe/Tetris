f0d1e1d94ff7ef407f993c2446bfa245e9b42452
SireConflexe <valentin.dty@gmail.com>
2019-04-19 17:46:13 +0200

    list test

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -4,7 +4,7 @@ package test;
 //import static org.junit.Assert.assertThat;
 
 import  org.junit.Assert;	
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;	
 
 import main.Board;
 import main.Game;
@@ -19,7 +19,45 @@ public class GameTest {
 		
 		
 		Game g = new Game();
-		assertThat(g.getBoard()).isEqualTo(new Board(10,24));
+	//	assertThat(g.getBoard()).isEqualTo(new Board(10,24));
+		
+		
+		//si keyboard pressé, changement de la currentform
+		
+		//si gameover, pas de nouvelle form
+		
+		//si création de game, plateau de taille 10x24
+		
+		//si création de game, première pièce apparait
+		
+		//si on ne touche à rien, la pièce descend (toutes les 1 secondes,  on utilise le thread.sleep)
+		
+		//si collision, la pièce ne bouge plus
+		
+		//si piece ne bouge pas, nouvelle piece
+		
+		//si keyboard pressé, piece vers le bas
+		
+		//si keyboard pressé, bouger horizontallement
+		
+		//si bloc fixé en haut, game over nullos
+		
+		
+		// BONUS : 2/4
+		
+		//si game lancé, affichage lancé (tester le jframe)
+		
+		// si keyboard pressé, drop the piece
+		
+		/* 
+		 * 
+		 * dans l'affichage, je me charges de rajouter le temps écoulé et le score ;-)
+		 * 
+		 * le changement de lvl se fait aussi, clairement
+		 * 
+		 * chercher affichage correct
+		 */
+		 
 		
 		
 	}
