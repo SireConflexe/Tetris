b1fcbb5a6d1f8d6dbe2a81973c4a0b4adc4b9e7f
Thibaud Germain <39822686+Grimnir777@users.noreply.github.com>
2019-04-23 22:51:23 +0200

    refactoring gameTest

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -1,55 +1,15 @@
 package test;
 
-
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.awt.GraphicsConfiguration;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
-import javax.swing.JFrame;
-
-import  org.junit.Assert;	
 import org.junit.jupiter.api.Test;	
-
-import main.Board;
 import main.Game;
 
-
-
-
 public class GameTest {
-
-
-	@Test
-	public void should_goDown_when_nothingHappens() {
-		
-		Game g = new Game();
-		Board b = g.boardgame;
-
-		CountDownLatch lock = new CountDownLatch(1);
-		
-		try {
-			lock.await(2000,TimeUnit.MILLISECONDS);
-
-	//		assertNotEquals(g.boardgame,b);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-			
-	}
-	
-	
 	@Test
 	public void should_displayGame_when_gameLaunching() {
-		
 		Game g = new Game();
-		
 		assertEquals(g.frame.isVisible(),true);
 	}
-	
 
 	@Test
 	public void should_have100points_when_aLineIsOver() {
@@ -62,6 +22,4 @@ public class GameTest {
 		Game g = new Game();
 		assertEquals(900,g.getNbPoints(5));
 	}
-		
-	
 }
